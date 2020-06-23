42c7d8cabff4b882d0b46a830b24fe6ad310af5a
Caio Valtécio <c.valtecio@gmail.com>
2020-06-23 15:40:31 -0300

    configuring firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBLIGJD0PN0epSwFM7nSEqdzTgB_T07rbc",
+    authDomain: "location-monitoring.firebaseapp.com",
+    databaseURL: "https://location-monitoring.firebaseio.com",
+    projectId: "location-monitoring",
+    storageBucket: "location-monitoring.appspot.com",
+    messagingSenderId: "519369718872",
+    appId: "1:519369718872:web:5e3dd8394cae2099435e2d",
+    measurementId: "G-BMSML0RNJF"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBLIGJD0PN0epSwFM7nSEqdzTgB_T07rbc",
+    authDomain: "location-monitoring.firebaseapp.com",
+    databaseURL: "https://location-monitoring.firebaseio.com",
+    projectId: "location-monitoring",
+    storageBucket: "location-monitoring.appspot.com",
+    messagingSenderId: "519369718872",
+    appId: "1:519369718872:web:5e3dd8394cae2099435e2d",
+    measurementId: "G-BMSML0RNJF"
+  }
 };
 
 /*
