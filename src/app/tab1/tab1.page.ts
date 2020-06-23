b5068f0131c97026879da95509f7a13345fa0664
Caio Valtécio <c.valtecio@gmail.com>
2020-06-23 15:43:11 -0300

    creating page to display locations

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from '../services/geolocation/geolocation.service';
+import { FirebaseService } from '../services/firebase/firebase.service';
+import { Plugins } from '@capacitor/core';
+import { Observable } from 'rxjs';
+import { AngularFireAction } from '@angular/fire/database';
+
+const { Device } = Plugins;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public currentPosition: any = [];
+  public watchPosition: any[] = [];
+  public locations$: Observable<any[]>;
+
+  constructor(
+    private geolocation: GeolocationService,
+    private firebase: FirebaseService
+  ) { }
+
+  ngOnInit() {
+    this.getCurrentPosition();
+    this.getWatchPosition();
+    this.getLocations();
+  }
+
+  async getCurrentPosition() {
+    this.currentPosition = await this.geolocation.getCurrentPosition();
+  }
+
+  async getWatchPosition() {
+    const watch = await this.geolocation.watchPosition();
+  }
+
+  async getLocations() {
+    const info = await Device.getInfo();
+    this.firebase.getQuery(info.uuid)
+      .subscribe((data: any) => this.locations$ = data.reverse());
+  }
+
+
 
 }
